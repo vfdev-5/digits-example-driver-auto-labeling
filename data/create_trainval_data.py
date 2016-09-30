37c4e302d223eb4e2c76d4ec83254ef4a5591c32
Victor Fomin <victor.fomin@diginext.local>
2016-09-30 17:11:54 +0200

    * Update script to create data coherent with DIGITS * Update data

--- data/create_trainval_data.py
+++ data/create_trainval_data.py
@@ -66,27 +66,71 @@ def get_annotations(filename):
                 labels.append(item)
     return np.array(labels)
    
-def write_images_labels(annotations, data_path, output_path):
+def write_images_labels(annotations, data_path, output_path, image_size):
+    """ 
+        LABEL STRUCTURE from DIGITS\digits\extensions\data\objectDetection\utils.py
+    
+        This class is the data ground-truth
+
+        #Values    Name      Description
+        ----------------------------------------------------------------------------
+        1    type         Class ID
+        1    truncated    Float from 0 (non-truncated) to 1 (truncated), where
+                          truncated refers to the object leaving image boundaries.
+                          -1 corresponds to a don't care region.
+        1    occluded     Integer (-1,0,1,2) indicating occlusion state:
+                          -1 = unkown, 0 = fully visible,
+                          1 = partly occluded, 2 = largely occluded
+        1    alpha        Observation angle of object, ranging [-pi..pi]
+        4    bbox         2D bounding box of object in the image (0-based index):
+                          contains left, top, right, bottom pixel coordinates
+        3    dimensions   3D object dimensions: height, width, length (in meters)
+        3    location     3D object location x,y,z in camera coordinates (in meters)
+        1    rotation_y   Rotation ry around Y-axis in camera coordinates [-pi..pi]
+        1    score        Only for results: Float, indicating confidence in
+                          detection, needed for p/r curves, higher is better.
+
+        Here, 'DontCare' labels denote regions in which objects have not been labeled,
+        for example because they have been too far away from the laser scanner.
+    """
+
     
     output_images_folder = os.path.join(output_path, "images")
     output_labels_folder = os.path.join(output_path, "labels")
     os.makedirs(output_images_folder)
     os.makedirs(output_labels_folder)
+    
+    def _clamp(x, dim):
+        return min(max(x, 0), dim-1)
         
     for i,annotation in enumerate(annotations):
         img_filename = annotation['filename']
         basename, ext = os.path.splitext(os.path.basename(img_filename))
         src_image_filename = os.path.join(data_path, img_filename)
-        #dst_image_filename = os.path.join(output_images_folder, "%s%s" % (i,ext))
-        dst_image_filename = os.path.join(output_images_folder, basename+ext)
+        dst_image_filename = os.path.join(output_images_folder, "%s%s" % (i,ext))
+        #dst_image_filename = os.path.join(output_images_folder, basename+ext)
         shutil.copyfile(src_image_filename, dst_image_filename)
-        #dst_label_filename = os.path.join(output_labels_folder, "%s.txt" % i)
-        dst_label_filename = os.path.join(output_labels_folder, basename + ".txt")
+        dst_label_filename = os.path.join(output_labels_folder, "%s.txt" % i)
+        #dst_label_filename = os.path.join(output_labels_folder, basename + ".txt")
         with open(dst_label_filename, 'w') as writer:
             for obj in annotation['annotations']:
                 # format : class_name bbox_left bbox_top bbox_right bbox_bottom
                 l, t, w, h = int(obj['x']), int(obj['y']), int(obj['width']), int(obj['height'])
-                line = "%s %s %s %s %s\n" % (obj['class'], l, t, l+w, t-h)
+                r = l+w; b = t+h
+                l = _clamp(l, image_size[0])
+                t = _clamp(t, image_size[1])
+                r = _clamp(r, image_size[0])
+                b = _clamp(b, image_size[1])
+                line = "{type} {truncated} {occluded} {alpha} {l} {t} {r} {b} {h} {w} {le} {x} {y} {z} {ry}\n".format(
+                    type=obj['class'],
+                    truncated=0.0,
+                    occluded=-1,
+                    alpha=0.0,
+                    l=l, t=t, r=r, b=b,
+                    h=0.0, w=0.0, le=0.0,
+                    x=0.0, y=0.0, z=0.0,
+                    ry = 0.0
+                )   
                 writer.write(line)
         
 if __name__ == "__main__":
@@ -97,7 +141,8 @@ if __name__ == "__main__":
 
     RAW_DATA_PATH = sys.argv[1]
     SLOTH_LABELS_PATH = sys.argv[2]
-    TRAIN_TEST_SPLIT=0.7
+    TRAIN_TEST_SPLIT=0.8
+    IMAGE_SIZE=[640, 480]
     
     annotations = get_annotations(SLOTH_LABELS_PATH)
     
@@ -120,6 +165,8 @@ if __name__ == "__main__":
     if os.path.isdir("test"):
         shutil.rmtree("test")
     
-    write_images_labels(train_annotations, RAW_DATA_PATH, "train")
-    write_images_labels(test_annotations, RAW_DATA_PATH, "test")
+    write_images_labels(train_annotations, RAW_DATA_PATH, "train", IMAGE_SIZE)
+    write_images_labels(test_annotations, RAW_DATA_PATH, "test", IMAGE_SIZE)
+    
+    print "Done."
 
